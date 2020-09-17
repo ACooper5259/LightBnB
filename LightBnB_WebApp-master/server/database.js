e866cd09fba69866565303ce771fbe0a92362210
vagrant <audrey_cooper@me.com>
2020-09-17 18:20:24 +0000

    database.js updated with queries onto live database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -23,17 +23,27 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query('SELECT * FROM users WHERE email = $1', [email])
+    .then ((response) => {
+      if (response.rows[0]) {
+        return response.rows[0]
+      } 
+      return null;
+    });
 }
+    
+
+  //   let user;
+//   for (const userId in users) {
+//     user = users[userId];
+//     if (user.email.toLowerCase() === email.toLowerCase()) {
+//       break;
+//     } else {
+//       user = null;
+//     }
+//   }
+//   return Promise.resolve(user);
+// }
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -42,7 +52,14 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query('SELECT * FROM users WHERE id = $1', [id])
+  .then ((response) => {
+    if (response.rows[0]) {
+      return response.rows[0]
+    } 
+    return null;
+  });
+  // return Promise.resolve(users[id]);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -53,10 +70,16 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query ( 'INSERT INTO users (name, email, password) VALUES ( $1,$2,$3) RETURNING *;', [user.name, user.email, user.password] )
+    .then ((response) => {
+      return (response.rows[0])
+    })
+  
+
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
 }
 exports.addUser = addUser;
 
@@ -86,7 +109,7 @@ const getAllProperties = function(options, limit = 10) {
   LIMIT $1
   `, [limit])
   .then(res => {
-    returnsq(res.rows)
+    return(res.rows)
   });
 }
 exports.getAllProperties = getAllProperties;
