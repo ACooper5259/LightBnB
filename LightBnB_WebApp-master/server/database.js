291c245315216714d7c8319a240cf292f32ffb6d
Audrey Cooper <audreycooper@Audreys-MacBook-Pro.local>
2020-09-17 16:09:24 -0700

    Requirement: add new property listing to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -112,7 +112,7 @@ const getAllProperties = function(options, limit = 10) {
   }
 
   if (options.owner_id) {
-    queryParams.push(`%${options.owner_id}%`);
+    queryParams.push(`${options.owner_id}`);
     queryString += `WHERE owner_id = $${queryParams.length} `;
   }
 
@@ -134,7 +134,7 @@ const getAllProperties = function(options, limit = 10) {
   `;
 
   // 5
-  console.log(queryString, queryParams);
+  // console.log(queryString, queryParams);
 
   // 6
   return pool.query(queryString, queryParams)
@@ -149,10 +149,29 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+  
+  return pool.query (`INSERT INTO properties (
+    owner_id, 
+    title, 
+    description, 
+    thumbnail_photo_url, 
+    cover_photo_url, 
+    cost_per_night, 
+    parking_spaces, 
+    number_of_bathrooms, 
+    number_of_bedrooms, 
+    country, 
+    street, 
+    city, 
+    province, 
+    post_code)
+    VALUES ( $1,$2,$3, $4, $5, $6, $7, $8, $9, $10, $11, $12,$13, $14) RETURNING *;`,
+    [property.owner_id, property.title, property.descritpion, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code] )
+    .then ((response) => {
+      console.log (response)
+      return (response.rows[0])
+    });
+};
+
 exports.addProperty = addProperty;
 
